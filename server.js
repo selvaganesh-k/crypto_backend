7f839784f1685fd193f2793d5124dc93b38488cb
selvaganeshDC <selvaganesh.deecodes@gmail.com>
2025-09-17 12:57:55 +0530

    latest

---
+++ server.js
@@ -0,0 +1,25 @@
+import express, {} from "express";
+import axios from "axios";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.get("/api/coins", async (req, res) => {
+    try {
+        const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets", {
+            params: {
+                vs_currency: "usd",
+                order: "market_cap_desc",
+                per_page: 50,
+                page: 1,
+                sparkline: false,
+            },
+        });
+        res.json(response.data);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Error fetching crypto data" });
+    }
+});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
+//# sourceMappingURL=server.js.map
